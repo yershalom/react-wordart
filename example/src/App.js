cb329b8669666084f192f8c91b8b326037f95b0e
Shalom Yerushalmy <yershalom@gmail.com>
2019-01-31 22:37:37 +0200

    Add star me on github

--- example/src/App.js
+++ example/src/App.js
@@ -4,6 +4,18 @@ import WordArt from 'react-wordart'
 
 export default class App extends Component {
   render () {
-    return WordArt.themes.map(theme => <div style={{padding: 30}} key={theme}><WordArt text={theme} theme={theme} /></div>)
+    return (
+      <div>
+        <a className='github-button'
+          href='https://github.com/yershalom/react-wordart'
+          data-size='large'
+          data-show-count='true'
+          aria-label='Star yershalom/react-wordart on GitHub'
+        >
+          Star
+        </a>
+        {WordArt.themes.map(theme => <div style={{padding: 30}} key={theme}><WordArt text={theme} theme={theme} /></div>)}
+      </div>
+    )
   }
 }
