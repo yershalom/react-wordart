30381ff60c37c70a6e79dbf7144c4659098b032f
Itamar Sharify <itamarsharify@gmail.com>
2019-02-07 23:10:08 +0200

    Prevent locking for certain styles
    This will help preventing the need to update the library any time the wordart-css is updated

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import WordArt from 'react-wordart'
-
+import WordArt from "react-wordart";
+const themes = [
+  "rainbow",
+  "blues",
+  "superhero",
+  "radial",
+  "tilt",
+  "purple",
+  "horizon",
+  "italicOutline",
+  "slate"
+];
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <a className='github-button'
-          href='https://github.com/yershalom/react-wordart'
-          data-size='large'
-          data-show-count='true'
-          aria-label='Star yershalom/react-wordart on GitHub'
+        <a
+          className="github-button"
+          href="https://github.com/yershalom/react-wordart"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star yershalom/react-wordart on GitHub"
         >
           Star
         </a>
-        {WordArt.themes.map(theme => <div style={{padding: 30}} key={theme}><WordArt text={theme} theme={theme} /></div>)}
+        {themes.map(theme => (
+          <div style={{ padding: 30 }} key={theme}>
+            <WordArt text={theme} theme={theme} />
+          </div>
+        ))}
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,44 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames/bind'
-import styles from 'css-wordart'
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames/bind";
+import styles from "css-wordart";
 
-const themes = {
-  rainbow: 'rainbow',
-  blues: 'blues',
-  superhero: 'superhero',
-  radial: 'radial',
-  tilt: 'tilt',
-  purple: 'purple',
-  horizon: 'horizon',
-  italicOutline: 'italicOutline',
-  slate: 'slate'
-}
-
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 const WordArt = ({ text, theme, fontSize }) => {
-  const th = themes[theme]
-  const className = cx('wordart', th)
+  const th = theme;
+  const className = cx("wordart", th);
 
   return (
-    <div className={className} style={{fontSize}} >
+    <div className={className} style={{ fontSize }}>
       <span className={styles.text}>{text}</span>
     </div>
-  )
-}
+  );
+};
 
 WordArt.propTypes = {
   text: PropTypes.string.isRequired,
   theme: PropTypes.oneOf(Object.keys(themes)),
   fontSize: PropTypes.number
-}
+};
 
 WordArt.defaultProps = {
-  theme: 'rainbow',
+  theme: "rainbow",
   fontSize: 50
-}
+};
 
-WordArt.themes = Object.keys(themes)
-
-export default WordArt
+export default WordArt;
